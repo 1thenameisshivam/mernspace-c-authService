0dac2247b834fcf1be44fe0e791e80c4f7295dc8
1thenameisshivam <1thenameisshivam@gmail.com>
2025-01-21 22:27:10 +0530

    add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+// Resolve the directory name
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.__dirname,
+            },
+        },
+        rules: {
+            // "no-console": "error",
+            //"dot-notation": "error",
+        },
+    },
+);
--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ function welcome(name: string) {
     console.log("welcome " + name);
 }
 
+const user = {
+    name: "shivam",
+    age: 25,
+};
+
+console.log(user.name);
+
 welcome("shivam");
